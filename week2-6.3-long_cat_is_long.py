7253b3e0b3e420175e0e3a756f5046d1b4859cdd
Anton Nahhas <antonna@edu.hac.ac.il>
2023-03-28 16:37:13 +0300

    added ex 6.3

---
+++ week2-6.3-long_cat_is_long.py
@@ -0,0 +1,25 @@
+import re
+
+
+def count_words(text):
+    """
+    this function receives a string, uses a generator expression to remove the all special characters
+    and store all the words in a list, then uses a dictionary comprehension to loop through that list
+    and store the name and the length of each word and returns it to be printed, the program imports
+    regular expression module to extract words only from the text.
+    :param text: a string to operate on
+    :return: a dictionary that has all the words from the text as keys and the values are the length of each word
+    """
+    clean = (word for word in re.split("\W+", text) if word.isalpha())
+    result = {word.lower(): len(word) for word in clean}
+
+    return result
+
+
+print(count_words("""
+You see, wire telegraph is a kind of a very, very long cat.
+You pull his tail in New York and his head is meowing in Los Angeles.
+Do you understand this?
+And radio operates exactly the same way: you send signals here, they receive them there.
+The only difference is that there is no cat.
+"""))
