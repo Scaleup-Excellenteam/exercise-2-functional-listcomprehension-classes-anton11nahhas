50605fa168b477f622c6a699052a52a8ccb8baa8
Anton Nahhas <antonna@edu.hac.ac.il>
2023-03-28 16:41:44 +0300

    added ex 6.4

---
+++ week2-6.4-remember_remember.py
@@ -0,0 +1,26 @@
+import PIL
+from PIL import Image
+
+
+def remember(path):
+    """
+    this program imports pil (pillow) module that can manipulates and operates on images
+    this function opens the given image and loads its pixels onto a matrix, loop through
+    the matrix and for each pixel check if the pixel is black and change the row num of the
+    black pixel to its char form and extract the hidden message.
+    for the code.png image the output should be:
+    Place gunpowder beneath the House of Lords. 11/05/1605
+    :param path: path for the image
+    :return: returns the extracted hidden message
+    """
+    result = ""
+    img = PIL.Image.open(path)
+    pix = img.load()
+    for x in range(img.size[0]):
+        for y in range(img.size[1]):
+            if pix[x, y] == 1:
+                result += chr(y)
+    return result
+
+
+print(remember('resources/code.png'))
