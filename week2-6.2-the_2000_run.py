eb5c8cacaec8f13b9ecd0ec81f5ba14d93df359f
Anton Nahhas <antonna@edu.hac.ac.il>
2023-05-13 13:43:44 +0300

    added a main function, imporved variable names and made time calculations more accurate

--- week2-6.2-the_2000_run.py
+++ week2-6.2-the_2000_run.py
@@ -1,20 +1,26 @@
 import time
 
 
-def timer(f, *args, **kwargs):
+def timer(function, *args, **kwargs):
     """
     this function receives a pointer to another function and its params,
     the program imports time module so the function can compute the run
     time of that function, and returns it.
-    :param f: this is a pointer to function
-    :param p: multiple parameters
+    :param function: this is a pointer to function
     :return: the time taken to undergo the function
     """
 
-    start = time.time()
-    f(*args, **kwargs)
-    end = time.time()
+    start = time.perf_counter()
+    function(*args, **kwargs)
+    end = time.perf_counter()
+
     return end - start
 
 
-print(timer(print, "Hello"))
+def main():
+    result = timer(print, "Hello")
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
