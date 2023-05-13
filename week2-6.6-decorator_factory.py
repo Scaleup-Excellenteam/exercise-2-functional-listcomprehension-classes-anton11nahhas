18bc90f4123a1afeb84455707a4c1dfce19beb3d
Anton Nahhas <antonna@edu.hac.ac.il>
2023-05-13 15:11:37 +0300

    added a main function, implemented the empty class, and added documentations

--- week2-6.6-decorator_factory.py
+++ week2-6.6-decorator_factory.py
@@ -1,11 +1,15 @@
 from functools import wraps
 
 
-class MsgError(Exception):
+class TypeCheckError(TypeError):
     """
-    a message error class for error messages if raised.
+    Custom error raised when an argument has an unexpected type.
     """
-    pass
+
+    def __init__(self, arg_name, expected_type):
+        self.arg_name = arg_name
+        self.expected_type = expected_type
+        super().__init__(f"{arg_name} should be of type {expected_type.__name__}")
 
 
 def type_check(correct_type):
@@ -17,19 +21,13 @@ def type_check(correct_type):
     :param correct_type: our correct type, in this case is INT
     :return: returns a decorator
     """
+
     def decorator(func):
         @wraps(func)
-        def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            try:
-                if type(result) != correct_type:
-                    raise MsgError(f"the type: {type(result)}, is not the correct one.")
-
-            except MsgError as msg:
-                print(msg)
-                result = " "
-
-            return result
+        def wrapper(arg, *args, **kwargs):
+            if not isinstance(arg, correct_type):
+                raise TypeCheckError(func.__name__, correct_type)
+            return func(arg, *args, **kwargs)
 
         return wrapper
 
@@ -38,7 +36,18 @@ def type_check(correct_type):
 
 @type_check(int)
 def times_2(num):
+    """
+    A function that takes an integer and returns its double value.
+    :param num: variable that must be tested
+    :return: the double of the input value
+    """
     return num * 2
 
 
-print(times_2("5"))
+def main():
+    print(times_2(5))  # output: success
+    print(times_2("5"))  # output: exception
+
+
+if __name__ == "__main__":
+    main()
